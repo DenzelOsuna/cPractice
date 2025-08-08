f0a5fd70f69be4f3787d7f27e1f93c80ea90c805
DenzelOsuna <denzelos2016@gmail.com>
2025-08-07 23:01:25 -0600

    Arithmetic practice

---
+++ 09.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() {
+  int a, b, c, d, e, f;
+
+  a = 5;
+  b = 2;
+  c = a + b;
+  d = a - b;
+  e = a / b;
+  f = a % b;
+  printf("Resultado de suma: %d", c);
+  printf("\n");
+  printf("resulato de resta: %d", d);
+  printf("\n");
+  printf("resultado de division: %d", e);
+  printf("\n");
+  printf("resultado de modulo: %d", f);
+}
