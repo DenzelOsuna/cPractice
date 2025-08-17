7963d910f73d9f675b9c5027ffbfff6f757f0597
DenzelOsuna <denzelos2016@gmail.com>
2025-08-17 14:30:45 -0600

    While loop

---
+++ 010.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+  int numero = 0;
+  int limite = 3;
+  while (numero < limite) {
+    printf("iteracion?> %d", numero);
+    printf("\n");
+    numero = numero + 1;
+  }
+}
